70ca71b8bae1f3b8fb93d20b329518870e77a27d
Luiz Henrique Martins <lz_elitemetal@hotmail.com>
2023-03-28 23:25:17 -0300

    init

---
+++ public/js/main.js
@@ -0,0 +1,242 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const listaPokemons = document.getElementById("lista-pokemons");
+const slots = document.querySelectorAll(".slot");
+const btnChoose = document.getElementById("choose");
+const btnCompartilhar = document.getElementById("compartilhar");
+const btnGerarAleatorio = document.getElementById("gerar-aleatorio");
+const btnGerarTimeAleatorio = document.getElementById("gerar-time-aleatorio");
+const filtro = document.getElementById("filtro");
+const btnReset = document.getElementById("reset");
+function carregarListaDePokemons() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+            const pokemons = response.data.results;
+            for (let pokemon of pokemons) {
+                const option = document.createElement("option");
+                option.innerText = pokemon.name;
+                listaPokemons.appendChild(option);
+            }
+            //listaPokemons.addEventListener("change", escolherPokemon);
+            btnChoose.addEventListener("click", escolherPokemon);
+            btnCompartilhar.addEventListener("click", compartilharTime);
+            btnGerarAleatorio.addEventListener("click", gerarPokemonAleatorio);
+            btnGerarTimeAleatorio.addEventListener("click", gerarTimeAleatorio);
+            btnReset.addEventListener("click", resetarTime);
+            // Adicione o ouvinte de eventos para o campo de busca
+            ///filtro.addEventListener("input", filtrarPokemons);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+function escolherPokemon(event) {
+    return __awaiter(this, void 0, void 0, function* () {
+        event.preventDefault();
+        let pokemonName = filtro.value;
+        if (!pokemonName) {
+            alert("Por favor, selecione um Pokémon.");
+            return;
+        }
+        try {
+            const response = yield axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+            console.log(response);
+            const pokemonImg = response.data.sprites.front_default;
+            var pokemonWeight = `${response.data.weight} Kg`;
+            var pokemonHeight = `${response.data.height}0 Cm`;
+            var pokemonType = "";
+            for (var type in response.data["types"]) {
+                pokemonType += `TYPE:${response.data["types"][type].type.name} `;
+            }
+            var pokemonHp = `HP:${response.data["stats"][0]["base_stat"]}`;
+            var pokemonAttack = `ATK:${response.data["stats"][1]["base_stat"]}`;
+            var pokemonDefense = `DEF:${response.data["stats"][2]["base_stat"]}`;
+            var pokemonEspecialAttack = `ESP-ATK:${response.data["stats"][3]["base_stat"]}`;
+            var pokemonEspecialDefense = `ESP-DEF:${response.data["stats"][4]["base_stat"]}`;
+            var pokemonSpeed = `SPEED:${response.data["stats"]["5"]["base_stat"]}`;
+            let timeCompleto = true;
+            pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+            for (let slot of slots) {
+                if (!slot.querySelector('.infos').innerHTML) {
+                    slot.querySelector('.pokename').innerHTML = ` ${pokemonName}`;
+                    slot.querySelector('.infos').innerHTML = `
+        pokemonWeight: ${pokemonWeight}
+        pokemonHeight: ${pokemonHeight}
+          pokemonType: ${pokemonType}
+          pokemonHp: ${pokemonHp}
+          pokemonAttack: ${pokemonAttack}
+          pokemonDefense: ${pokemonDefense}
+          pokemonEspecialAttack: ${pokemonEspecialAttack}
+          pokemonEspecialDefense: ${pokemonEspecialDefense}
+          pokemonSpeed: ${pokemonSpeed}
+                    `;
+                    slot.querySelector('img').src = pokemonImg;
+                    slot.querySelector('img').alt = pokemonName;
+                    timeCompleto = false;
+                    break;
+                }
+            }
+            if (timeCompleto) {
+                if (navigator.vibrate) {
+                    navigator.vibrate(200);
+                }
+                else {
+                    tremor(document.body);
+                }
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+// Função para resetar os slots
+function resetarTime() {
+    let temPokemon = false;
+    for (let slot of slots) {
+        if (slot.innerHTML) {
+            temPokemon = true;
+            slot.querySelector('span').innerText = '';
+            slot.querySelector('img').src = '';
+            slot.querySelector('img').alt = '';
+        }
+    }
+    if (!temPokemon) {
+        alert("Nenhum Pokémon foi gerado ainda.");
+    }
+}
+// Função para compartilhar o time de Pokémon usando a Web Share API
+function compartilharTime() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const time = Array.from(slots).map(slot => slot.innerText).join(', ');
+        if (navigator.share) {
+            try {
+                yield navigator.share({
+                    title: 'Meu Time de Pokémon',
+                    text: `Confira o meu time de Pokémon: ${time}`,
+                    url: window.location.href,
+                });
+                console.log('Time compartilhado com sucesso!');
+            }
+            catch (error) {
+                console.error('Erro ao compartilhar:', error);
+            }
+        }
+        else {
+            alert('Web Share API não é suportada neste navegador.');
+        }
+    });
+}
+// Função para gerar um Pokémon aleatório
+// Função para gerar um Pokémon aleatório
+function gerarPokemonAleatorio() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const randomIndex = Math.floor(Math.random() * 151) + 1;
+        try {
+            const response = yield axios.get(`https://pokeapi.co/api/v2/pokemon/${randomIndex}`);
+            const pokemonName = response.data.name;
+            const pokemonImg = response.data.sprites.front_default;
+            var pokemonWeight = `${response.data.weight} Kg`;
+            var pokemonHeight = `${response.data.height}0 Cm`;
+            var pokemonType = "";
+            for (var type in response.data["types"]) {
+                pokemonType += `TYPE:${response.data["types"][type].type.name} `;
+            }
+            var pokemonHp = `HP:${response.data["stats"][0]["base_stat"]}`;
+            var pokemonAttack = `ATK:${response.data["stats"][1]["base_stat"]}`;
+            var pokemonDefense = `DEF:${response.data["stats"][2]["base_stat"]}`;
+            var pokemonEspecialAttack = `ESP-ATK:${response.data["stats"][3]["base_stat"]}`;
+            var pokemonEspecialDefense = `ESP-DEF:${response.data["stats"][4]["base_stat"]}`;
+            var pokemonSpeed = `SPEED:${response.data["stats"]["5"]["base_stat"]}`;
+            let timeCompleto = true;
+            for (let slot of slots) {
+                if (!slot.querySelector('.infos').innerHTML) {
+                    slot.querySelector('.pokename').innerHTML = ` ${pokemonName}`;
+                    slot.querySelector('.infos').innerHTML = `
+         <p> pokemonWeight: ${pokemonWeight}
+          pokemonHeight: ${pokemonHeight}
+            pokemonType: ${pokemonType}
+            pokemonHp: ${pokemonHp}
+            pokemonAttack: ${pokemonAttack}
+            pokemonDefense: ${pokemonDefense}
+            pokemonEspecialAttack: ${pokemonEspecialAttack}
+            pokemonEspecialDefense: ${pokemonEspecialDefense}
+            pokemonSpeed: ${pokemonSpeed}`;
+                    slot.querySelector('img').src = pokemonImg;
+                    slot.querySelector('img').alt = pokemonName;
+                    timeCompleto = false;
+                    break;
+                }
+            }
+            if (timeCompleto) {
+                if (navigator.vibrate) {
+                    navigator.vibrate(200);
+                }
+                else {
+                    tremor(document.body);
+                }
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+// Função para gerar um time de Pokémon aleatório
+function gerarTimeAleatorio() {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let slot of slots) {
+            slot.querySelector('.infos').innerHTML = '';
+            slot.querySelector('img').src = '';
+            slot.querySelector('img').alt = '';
+        }
+        for (let i = 0; i < 6; i++) {
+            yield gerarPokemonAleatorio();
+        }
+    });
+}
+carregarListaDePokemons();
+// Função para fazer a tela tremer com um efeito CSS
+function tremor(element) {
+    element.classList.add("tremer");
+    setTimeout(() => {
+        element.classList.remove("tremer");
+    }, 820);
+}
+/*
+function filtrarPokemons() {
+  const termo = filtro.value.toLowerCase();
+  const options = listaPokemons.options;
+
+  let encontrouPokemon = false;
+
+  for (let i = 0; i < options.length; i++) {
+    const pokemonName = options[i].innerText.toLowerCase();
+    if (pokemonName.startsWith(termo)) {
+      options[i].style.display = '';
+      if (!encontrouPokemon) {
+        listaPokemons.selectedIndex = i;
+        encontrouPokemon = true;
+      }
+    } else {
+      options[i].style.display = 'none';
+    }
+  }
+
+  if (encontrouPokemon) {
+    listaPokemons.style.display = 'block';
+  } else {
+    listaPokemons.style.display = 'none';
+  }
+}
+*/
---
+++ src/ts/main.ts
@@ -0,0 +1,240 @@
+const listaPokemons = document.getElementById("lista-pokemons") as HTMLSelectElement;
+const slots = document.querySelectorAll(".slot") as NodeListOf<HTMLDivElement>;
+const btnChoose = document.getElementById("choose") as HTMLButtonElement;
+const btnCompartilhar = document.getElementById("compartilhar") as HTMLButtonElement;
+const btnGerarAleatorio = document.getElementById("gerar-aleatorio") as HTMLButtonElement;
+const btnGerarTimeAleatorio = document.getElementById("gerar-time-aleatorio") as HTMLButtonElement;
+const filtro = document.getElementById("filtro") as HTMLInputElement;
+const btnReset = document.getElementById("reset") as HTMLButtonElement;
+async function carregarListaDePokemons() {
+  try {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const pokemons = response.data.results;
+
+    for (let pokemon of pokemons) {
+      const option = document.createElement("option");
+      option.innerText = pokemon.name;
+      listaPokemons.appendChild(option);
+
+    }
+
+    //listaPokemons.addEventListener("change", escolherPokemon);
+    btnChoose.addEventListener("click", escolherPokemon);
+    btnCompartilhar.addEventListener("click", compartilharTime);
+    btnGerarAleatorio.addEventListener("click", gerarPokemonAleatorio);
+    btnGerarTimeAleatorio.addEventListener("click", gerarTimeAleatorio);
+    btnReset.addEventListener("click", resetarTime);
+
+    // Adicione o ouvinte de eventos para o campo de busca
+    ///filtro.addEventListener("input", filtrarPokemons);
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function escolherPokemon(event : Event) {
+  event.preventDefault();
+
+  let pokemonName = filtro.value;
+
+  if (!pokemonName) {
+    alert("Por favor, selecione um Pokémon.");
+    return;
+  }
+
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    console.log(response);
+    const pokemonImg = response.data.sprites.front_default;
+    var pokemonWeight = `${response.data.weight} Kg`;
+    var pokemonHeight = `${response.data.height}0 Cm`;
+    var pokemonType = "";
+    for (var type in response.data["types"]) {
+       pokemonType += `TYPE:${response.data["types"][type].type.name} `;
+    }
+
+   var  pokemonHp = `HP:${response.data["stats"][0]["base_stat"]}`;
+   var  pokemonAttack = `ATK:${response.data["stats"][1]["base_stat"]}`;
+   var  pokemonDefense = `DEF:${response.data["stats"][2]["base_stat"]}`;
+   var  pokemonEspecialAttack = `ESP-ATK:${response.data["stats"][3]["base_stat"]}`;
+   var  pokemonEspecialDefense = `ESP-DEF:${response.data["stats"][4]["base_stat"]}`;
+    var pokemonSpeed = `SPEED:${response.data["stats"]["5"]["base_stat"]}`;
+    let timeCompleto = true;
+    pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+    for (let slot of slots) {
+      if (!slot.querySelector('.infos')!.innerHTML) {
+        slot.querySelector('.pokename')!.innerHTML = ` ${pokemonName}`;
+        slot.querySelector('.infos')!.innerHTML = `
+        pokemonWeight: ${pokemonWeight}
+        pokemonHeight: ${pokemonHeight}
+          pokemonType: ${pokemonType}
+          pokemonHp: ${pokemonHp}
+          pokemonAttack: ${pokemonAttack}
+          pokemonDefense: ${pokemonDefense}
+          pokemonEspecialAttack: ${pokemonEspecialAttack}
+          pokemonEspecialDefense: ${pokemonEspecialDefense}
+          pokemonSpeed: ${pokemonSpeed}
+                    `
+        slot.querySelector('img')!.src = pokemonImg;
+        slot.querySelector('img')!.alt = pokemonName;
+        timeCompleto = false;
+        break;
+      }
+    }
+
+    if (timeCompleto) {
+      if (navigator.vibrate) {
+        navigator.vibrate(200);
+      } else {
+        tremor(document.body);
+      }
+    }  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
+
+// Função para resetar os slots
+function resetarTime() {
+  let temPokemon = false;
+  for (let slot of slots) {
+    if (slot.innerHTML) {
+      temPokemon = true;
+      slot.querySelector('span')!.innerText = '';
+      slot.querySelector('img')!.src = '';
+      slot.querySelector('img')!.alt = '';
+    }
+  }
+
+  if (!temPokemon) {
+    alert("Nenhum Pokémon foi gerado ainda.");
+  }
+}
+ // Função para compartilhar o time de Pokémon usando a Web Share API
+async function compartilharTime() {
+  const time = Array.from(slots).map(slot => slot.innerText).join(', ');
+
+  if (navigator.share) {
+    try {
+      await navigator.share({
+        title: 'Meu Time de Pokémon',
+        text: `Confira o meu time de Pokémon: ${time}`,
+        url: window.location.href,
+      });
+      console.log('Time compartilhado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao compartilhar:', error);
+    }
+  } else {
+    alert('Web Share API não é suportada neste navegador.');
+  }
+}
+ // Função para gerar um Pokémon aleatório
+  // Função para gerar um Pokémon aleatório
+  async function gerarPokemonAleatorio() {
+    const randomIndex = Math.floor(Math.random() * 151) + 1;
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomIndex}`);
+      const pokemonName = response.data.name;
+      const pokemonImg = response.data.sprites.front_default;
+      var pokemonWeight = `${response.data.weight} Kg`;
+    var pokemonHeight = `${response.data.height}0 Cm`;
+    var pokemonType = "";
+    for (var type in response.data["types"]) {
+       pokemonType += `TYPE:${response.data["types"][type].type.name} `;
+    }
+   var  pokemonHp = `HP:${response.data["stats"][0]["base_stat"]}`;
+   var  pokemonAttack = `ATK:${response.data["stats"][1]["base_stat"]}`;
+   var  pokemonDefense = `DEF:${response.data["stats"][2]["base_stat"]}`;
+   var  pokemonEspecialAttack = `ESP-ATK:${response.data["stats"][3]["base_stat"]}`;
+   var  pokemonEspecialDefense = `ESP-DEF:${response.data["stats"][4]["base_stat"]}`;
+    var pokemonSpeed = `SPEED:${response.data["stats"]["5"]["base_stat"]}`;
+      let timeCompleto = true;
+      for (let slot of slots) {
+        if (!slot.querySelector('.infos')!.innerHTML) {
+          slot.querySelector('.pokename')!.innerHTML = ` ${pokemonName}`;
+
+          slot.querySelector('.infos')!.innerHTML = `
+         <p> pokemonWeight: ${pokemonWeight}
+          pokemonHeight: ${pokemonHeight}
+            pokemonType: ${pokemonType}
+            pokemonHp: ${pokemonHp}
+            pokemonAttack: ${pokemonAttack}
+            pokemonDefense: ${pokemonDefense}
+            pokemonEspecialAttack: ${pokemonEspecialAttack}
+            pokemonEspecialDefense: ${pokemonEspecialDefense}
+            pokemonSpeed: ${pokemonSpeed}`;
+          slot.querySelector('img')!.src = pokemonImg;
+          slot.querySelector('img')!.alt = pokemonName;
+          timeCompleto = false;
+          break;
+        }
+      }
+
+      if (timeCompleto) {
+        if (navigator.vibrate) {
+          navigator.vibrate(200);
+        } else {
+          tremor(document.body);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  // Função para gerar um time de Pokémon aleatório
+  async function gerarTimeAleatorio() {
+    for (let slot of slots) {
+      slot.querySelector('.infos')!.innerHTML = '';
+      slot.querySelector('img')!.src = '';
+      slot.querySelector('img')!.alt = '' ;    }
+
+    for (let i = 0; i < 6; i++) {
+      await gerarPokemonAleatorio();
+    }
+  }
+
+carregarListaDePokemons();
+
+
+// Função para fazer a tela tremer com um efeito CSS
+function tremor(element: HTMLElement) {
+  element.classList.add("tremer");
+  setTimeout(() => {
+    element.classList.remove("tremer");
+  }, 820);
+}
+
+
+
+/*
+function filtrarPokemons() {
+  const termo = filtro.value.toLowerCase();
+  const options = listaPokemons.options;
+
+  let encontrouPokemon = false;
+
+  for (let i = 0; i < options.length; i++) {
+    const pokemonName = options[i].innerText.toLowerCase();
+    if (pokemonName.startsWith(termo)) {
+      options[i].style.display = '';
+      if (!encontrouPokemon) {
+        listaPokemons.selectedIndex = i;
+        encontrouPokemon = true;
+      }
+    } else {
+      options[i].style.display = 'none';
+    }
+  }
+
+  if (encontrouPokemon) {
+    listaPokemons.style.display = 'block';
+  } else {
+    listaPokemons.style.display = 'none';
+  }
+}
+*/
